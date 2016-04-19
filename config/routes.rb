f1029cb34d2a429d9211829714b087a99f9ca1cd
jovennan <jovennan@gmail.com>
2016-04-19 14:36:50 -0300

    Adds devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-
+require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
+  devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },
                               constrains: { subdomain: 'api' }, path: '/' do
-    # We are going to list our resources here
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+        # We are going to list our resources here
+    end
   end
+  get 'home' => "sei_la#index"
 end
